d68dd2bbf1feef13f95c6df1814cd711ea7dbf1f
mert.alptekin <mert.alptekin@neomina.com>
2022-12-21 16:39:28 +0300

    Add project files.

---
+++ DemandManagement.Registration/RegisterDemandCommandConsumer.cs
@@ -0,0 +1,28 @@
+﻿using DemandManagement.MessageContracts;
+using GreenPipes;
+using MassTransit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DemandManagement.Registration
+{
+    public class RegisterDemandCommandConsumer : IConsumer<IRegisterDemandCommand>
+    {
+        public async Task Consume(ConsumeContext<IRegisterDemandCommand> context)
+        {
+            var message = context.Message;
+            var guid = Guid.NewGuid();
+            Console.WriteLine($"Demand successfully  registered. Subject:{message.Subject}, Description:{message.Description}, Id:{guid}, Time:{DateTime.Now}");
+            await Task.Delay(1000);
+      // fanout exchange kullanılıyor. eventi consume edenler eventi dinliyip işlem yapabilir.
+           await context.Publish<IRegisteredDemandEvent>(new
+            {
+                DemandId = guid
+            });
+
+        }
+    }
+}
